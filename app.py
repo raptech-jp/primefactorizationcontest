1bfe41797a2926d96bfe8dfe5e3f7d7094f0773f
tasaka <paparukisan@gmail.com>
2024-07-11 16:35:01 +0900

    [update] ほとんどの処理をサーバー側に変更

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import random
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.point'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # セッションを使用するために必要
+app.config['SESSION_TYPE'] = 'filesystem'
 
 db = SQLAlchemy(app)
+Session(app)
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,6 +27,8 @@ def index():
 
 @app.route("/game", methods=["GET"])
 def game():
+    session['score'] = 0
+    session['correct_count'] = 0
     return render_template("game/index.html")
 
 @app.route("/register", methods=["GET"])
@@ -31,14 +38,70 @@ def register_page():
 @app.route("/register", methods=["POST"])
 def register_process():
     username = request.form.get("username")
-    point = request.form.get("point")
+    point = session.get('score', 0)
     result = Score(username=username, point=point)
     db.session.add(result)
     db.session.commit()
     return redirect(url_for("index"))
+
+@app.route("/generate_primes", methods=["POST"])
+def generate_primes():
+    correct_count = session.get('correct_count', 0)
+
+    def is_prime(num):
+        if num <= 1:
+            return False
+        if num == 2:
+            return True
+        if num % 2 == 0:
+            return False
+        for i in range(3, int(num**0.5) + 1, 2):
+            if num % i == 0:
+                return False
+        return True
+
+    def generate_prime(max_num):
+        while True:
+            num = random.randint(1, max_num)
+            if is_prime(num):
+                return num
+
+    max_num = 10 + correct_count
+    prime1 = generate_prime(max_num)
+    prime2 = generate_prime(max_num)
+    product = prime1 * prime2
+
+    session['prime1'] = prime1
+    session['prime2'] = prime2
+
+    return jsonify({'product': product})
+
+@app.route("/check_primes", methods=["POST"])
+def check_primes():
+    data = request.json
+    prime1_input = data['prime1']
+    prime2_input = data['prime2']
+    product = data['product']
+    prime1 = session.get('prime1')
+    prime2 = session.get('prime2')
+    score = session.get('score', 0)
+    correct_count = session.get('correct_count', 0)
+
+    if (prime1_input == prime1 and prime2_input == prime2) or (prime1_input == prime2 and prime2_input == prime1):
+        if score < product:
+            score = product
+        correct_count += 1
+        result = '正解'
+    else:
+        result = '不正解'
     
+    session['score'] = score
+    session['correct_count'] = correct_count
+
+    return jsonify({'result': result, 'score': score, 'correct_count': correct_count})
+
 if __name__ == '__main__':
-    if not os.path.exists('instance/db.score'):
+    if not os.path.exists('instance/db.point'):
         with app.app_context():
             db.create_all()
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
